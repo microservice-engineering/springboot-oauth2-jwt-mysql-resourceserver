b612a24dc792b781451f37590f2c81c494f8e55b
Xiao Bo Li <lixiaobo@cn.ibm.com>
2018-01-27 08:48:20 +0800

    first commit

---
+++ src/main/java/com/resource/auth/MicroserviceResourceServerApplication.java
@@ -0,0 +1,12 @@
+package com.resource.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceResourceServerApplication.class, args);
+	}
+}
